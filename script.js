87e574c9b21c6e51737259aaa6c55b413f749f95
Rushwayc <rushwayc@yahoo.com>
2019-12-16 23:53:11 +0100

    atm idk what I'm doing

---
+++ script.js
@@ -0,0 +1,16 @@
+function onHover(i) {
+  const array = ["nav-stories", "nav-that", "nav-create", "nav-connections"];
+  const strings = ["MY CODING STORY", "IDK what to put here", "PROJECTS I CREATED", "MY CONNTACT INFO"];
+  const bckgUrls = ["url('images/bckgy.png", "url('images/bckglb.png')", "url('images/bckgp.png')", "url('images/bckgo.png')"];
+  var idvar = document.getElementById(array[i]);
+  idvar.innerHTML = `${strings[i]}`;
+	document.getElementById("bckgimg").style.backgroundImage = `${bckgUrls[i]}`;
+ }
+ 
+function outHover(i) {
+  const array = ["nav-stories", "nav-that", "nav-create", "nav-connections"];
+  const strings = ["STORIES", "THAT", "CREATE", "CONNECTIONS"];
+  var idvar = document.getElementById(array[i]);
+  idvar.innerHTML = `${strings[i]}`;
+	document.getElementById("bckgimg").style.backgroundImage = "url('images/bckg.png')";
+ }
