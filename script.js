1bb6ce14e69b85c70a0c2a8546d3d5ff3e455291
Rushwayc <rushwayc@yahoo.com>
2020-01-28 22:48:54 +0100

    It has been a busy January. Finished my first page from the navigation menu. Still need to upload a picture though. Started work on second page but I can barely keep my eyes open. Tried to force myself trough it but it is getting harder and harder. Gotta continue some other time.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function onHover(i) {
   const array = ["nav-stories", "nav-that", "nav-create", "nav-connections"];
-  const strings = ["MY CODING STORY", "IDK what to put here", "PROJECTS I CREATED", "MY CONNTACT INFO"];
+  const strings = ["MY CODING STORY", "LANGUAGES THAT I KNOW", "PROJECTS I CREATED", "MY CONNTACT INFO"];
   const bckgUrls = ["url('images/bckgy.png", "url('images/bckglb.png')", "url('images/bckgp.png')", "url('images/bckgo.png')"];
   var idvar = document.getElementById(array[i]);
   idvar.innerHTML = `${strings[i]}`;
